e2ad1529f78bd936d4c73f44a7f18e1b3ef73bd6
Rafay Ahmed <rafayahmed3003@gmail.com>
2023-05-15 12:50:44 -0700

    footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+$(document).ready(function () {
+    $('.slider').carousel({
+        num: 5,
+        maxWidth: 450,
+        maxHeight: 300,
+        distance: 50,
+        scale: 0.6,
+        animationTime: 1000,
+        showTime: 4000
+    });
 
-var vid = document.getElementById("player");
-
-function videoTimeUpdate(e) {
-    vid.setAttribute();
-}
-
-vid.addEventListener('timeupdate', videoTimeUpdate, false);
+});
